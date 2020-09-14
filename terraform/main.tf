06121c42153f8e750fbb68aed77909ae806ff75c
Pete Hodgson <git@thepete.net>
2020-09-13 21:38:14 -0700

    set up basic faux microservice

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,11 @@ resource "heroku_app" "storefront" {
   space            = heroku_space.default.name
   internal_routing = false
 
+  config_vars = {
+    DEALS_GATEWAY_BASE_URL = "http://${heroku_app.deals-service.name}.herokuapp.com"
+    DEBUG = "*"
+  }
+
   organization {
     name = var.heroku_enterprise_team
   }
@@ -24,4 +29,28 @@ resource "heroku_app" "storefront" {
   region = var.heroku_private_region
 }
 
+resource "heroku_formation" "storefront" {
+    app = heroku_app.storefront.name
+    type = "web"
+    quantity = 1
+    size = "private-s"
+}
 
+resource "heroku_app" "deals-service" {
+  name             = "${var.name}--deals"
+  space            = heroku_space.default.name
+  internal_routing = true
+
+  organization {
+    name = var.heroku_enterprise_team
+  }
+
+  region = var.heroku_private_region
+}
+
+resource "heroku_formation" "deals-service" {
+    app = heroku_app.deals-service.name
+    type = "web"
+    quantity = 1
+    size = "private-s"
+}
